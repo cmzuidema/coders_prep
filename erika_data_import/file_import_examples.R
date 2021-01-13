ac91ad1abf85adc22fad09c5808167d9ff881ddd
cmzuidema <chriszuidema@gmail.com>
2021-01-12 16:48:02 -0800

    initial edits -cz.

--- erika_data_import/file_import_examples.R
+++ erika_data_import/file_import_examples.R
@@ -1,10 +1,34 @@
-# File import examples
+# Import data example for DEOHS Coders Group 
+
 
 # Load packages, installing if needed
 if (!require(pacman)) install.packages("pacman")
-pacman::p_load(dplyr, tidyr, readxl)
+pacman::p_load(dplyr, tidyr, readxl, ggplot2)
 
-#Test
+# set working directory (Erika to modify)
+work_dir <- file.path(getwd(), "erika_data_import")
+setwd(work_dir)
+
+# Define data directory, creating if needed
+data_dir <- file.path(work_dir, "data")
+if (!dir.exists(data_dir)) {
+  dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
+}
+
+# name and create output directory
+output_dir <- file.path(work_dir, "output")
+dir.create(output_dir, showWarnings = TRUE, recursive = TRUE)
+
+
+# get xls file names 
+# (Erika: do you need any information in the file names? 
+# Modify file names to label dataframes?)
+file_names <- list.files(path = file.path(data_dir))
+df_names <- tibble(file_names) %>% 
+  separate(file_names, sep = "_|\\.", 
+           into = c("letter", "time", "instrument", "date"), 
+           extra = "drop") %>% 
+  mutate(name = paste(letter, instrument, sep = "_"))
 
 # Define functions for extracting metadata
 extract_metadata_sep <- function(path, range, sep = ":") {
@@ -17,38 +41,65 @@ extract_metadata <- function(path, range) {
         rename("var" = "...1", "val" = "...2")
 }
 
-# Define data directory, creating if needed
-data_dir <- "data"
-if (!dir.exists(data_dir)) {
-    dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
-}
+metadata_list <- lapply(file_names, function(i){
 
-# Define file path
-xls_file <- file.path(data_dir, "NaCL_25C_incubator test.xls")
-
-# Get metadata
-df_metadata <- rbind(extract_metadata_sep(xls_file, "A2"),
-                     extract_metadata(xls_file, "A5:B6"), 
-                     extract_metadata(xls_file, "D5:E6"),
-                     extract_metadata(xls_file, "A8:B9"),
-                     extract_metadata(xls_file, "A11:B14"), 
-                     extract_metadata(xls_file, "D11:E14"),
-                     extract_metadata(xls_file, "A21:B25"), 
-                     extract_metadata(xls_file, "D21:E25")) %>% 
+  # build file name
+  fn <- file.path(data_dir, i)
+  
+  # Get metadata
+  rbind(extract_metadata_sep(fn, "A2"),
+        extract_metadata(fn, "A5:B6"), 
+        extract_metadata(fn, "D5:E6"),
+        extract_metadata(fn, "A8:B9"),
+        extract_metadata(fn, "A11:B14"), 
+        extract_metadata(fn, "D11:E14"),
+        extract_metadata(fn, "A21:B25"), 
+        extract_metadata(fn, "D21:E25")) %>% 
     pivot_wider(names_from = "var", values_from = "val")
+})
+
+# assign metadata names
+names(metadata_list) <- df_names[["name"]]
 
 
 # Get Temp and RH data
-df <- read_xls(xls_file, skip = 26)
+df_list <- lapply(file_names, function(i){ 
+  
+  # build file name
+  fn <- file.path(data_dir, i) 
+  
+  # read files
+  read_xls(fn, skip = 26) %>% 
+    select(n = `No.`, time = Time, temp = `Temperature°C`, rh = `Humidity%RH`) %>% 
+    mutate(time = as.POSIXct(time, tz = "US/Pacific"))
+})
 
-# Alternate way to read metadata from XLS file using readxl and data.table
-pacman::p_load(readxl, data.table)
-df_meta_alt <- read_xls(xls_file, n_max = 25)
-df_meta_alt_1 <- data.frame(df_meta_alt[, 1:2])
-df_meta_alt_1[1, ] <- strsplit(df_meta_alt_1[1, 1], " on:")[[1]]
-setnames(df_meta_alt_1, names(df_meta_alt_1), c('var', 'val'))
-df_meta_alt_2 <- data.frame(df_meta_alt[, 4:5])
-setnames(df_meta_alt_2, names(df_meta_alt_2), c('var', 'val'))
-df_meta_alt <- rbind(df_meta_alt_1, df_meta_alt_2)
-df_meta_alt <- transpose(df_meta_alt[complete.cases(df_meta_alt),], 
-                         make.names = TRUE)
+# assign dataframe names (corresponding to metadata)
+names(df_list) <- df_names[["name"]]
+
+
+# create diagnostic plot
+figs <- lapply(names(df_list), function(i){ 
+  
+  # prepare data
+  temp <- df_list[[i]] %>% 
+    pivot_longer(cols = c("temp", "rh"), names_to = "parameter") 
+    
+    # create plot
+    ggplot(data = temp, aes(x = time, y = value)) + 
+      geom_line() + 
+      facet_wrap(~ parameter, ncol = 1, scales = "free_y") +
+      theme_bw()
+  
+})
+
+# assign figure names
+names(figs) <- df_names[["name"]]
+
+
+# save figures as ".png", ".jpg", or ".pdf" (and more) using lapply and ggsave
+# units can be specified as "in" or "cm"
+lapply(names(figs),function(x){
+  ggsave(filename = file.path(output_dir, paste0(x,".png")), plot=figs[[x]], 
+         width = 8, height = 5, units = "in", dpi = "print")
+})
