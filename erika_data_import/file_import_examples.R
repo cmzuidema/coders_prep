c6bd421bada8ef584bb8a440c18b36ab515fcd75
cmzuidema <chriszuidema@uw.edu>
2021-01-25 14:28:41 +0000

    cz final edits

--- erika_data_import/file_import_examples.R
+++ erika_data_import/file_import_examples.R
@@ -9,18 +9,15 @@ pacman::p_load(dplyr, tidyr, readxl, ggplot2, lubridate, rstatix, ggpubr)
 work_dir <- file.path(getwd(), "erika_data_import")
 setwd(work_dir)
 
-# Define data directory, creating if needed
+# define data directory
 data_dir <- file.path(work_dir, "data")
-##(!dir.exists(data_dir)) {
- ## dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
-## }
 
 # name and create output directory
 output_dir <- file.path(work_dir, "output")
 dir.create(output_dir, showWarnings = TRUE, recursive = TRUE)
 
 
-# get xls file names - Need information from file names 
+# get xls file names - need information from file names 
 file_names <- list.files(path = file.path(data_dir))
 df_names <- tibble(file_names) %>% 
   separate(file_names, sep = "_|\\.", 
@@ -28,7 +25,7 @@ df_names <- tibble(file_names) %>%
            extra = "drop") %>% 
   mutate(name = paste(letter, time, sep = "_"))
 
-# Define functions for extracting metadata
+# define two functions for extracting metadata
 extract_metadata_sep <- function(path, range, sep = ":") {
     read_xls(path = path, range = range, col_names = FALSE) %>%
         separate(col = 1, into = c('var', 'val'), sep = sep, extra = "merge")
@@ -39,12 +36,13 @@ extract_metadata <- function(path, range) {
         rename("var" = "...1", "val" = "...2")
 }
 
+# extract metadata with new functions
 metadata_list <- lapply(file_names, function(i){
 
   # build file name
   fn <- file.path(data_dir, i)
   
-  # Get metadata
+  # get metadata and bind together
   rbind(extract_metadata_sep(fn, "A2"),
         extract_metadata(fn, "A5:B6"), 
         extract_metadata(fn, "D5:E6"),
@@ -52,15 +50,17 @@ metadata_list <- lapply(file_names, function(i){
         extract_metadata(fn, "A11:B14"), 
         extract_metadata(fn, "D11:E14"),
         extract_metadata(fn, "A21:B25"), 
-        extract_metadata(fn, "D21:E25")) %>% 
-    pivot_wider(names_from = "var", values_from = "val")
-})
+        extract_metadata(fn, "D21:E25")) %>%
+    
+    # reshape data from long to wide
+    pivot_wider(names_from = "var", values_from = "val") 
+  }) %>% 
 
-# assign metadata names
-names(metadata_list) <- df_names[["name"]]
+  # assign metadata names
+  setNames(df_names[["name"]])
 
 
-# Get Temp and RH data
+# get Temp and RH data
 df_list <- lapply(file_names, function(i){ 
   
   # build file name
@@ -68,50 +68,51 @@ df_list <- lapply(file_names, function(i){
   
   # read files
   read_xls(fn, skip = 26) %>%
-    select(n = `No.`, time = Time, temp = `Temperature°C`, rh = `Humidity%RH`) %>%
-    mutate(time = as.POSIXct(time, tz = "GMT"),
-           timePST = force_tzs(time, tzones = "GMT", tzone_out = "US/Pacific"))
     
-})
+    # select columns and change names to remove special characters
+    select(n = `No.`, time = Time, temp = `Temperature°C`, rh = `Humidity%RH`) %>%
+    
+    # convert "time" to datetime class and change timezone
+    mutate(time = as.POSIXct(time, tz = "GMT"),
+           time_PST = force_tzs(time, tzones = "GMT", tzone_out = "US/Pacific"))
+  }) %>%
+  
+  # assign metadata names
+  setNames(df_names[["name"]])
 
 
-# assign dataframe names (corresponding to metadata)
-names(df_list) <- df_names[["name"]]
+# create diagnostic plots
+# make a figure list
+figs <- list()
 
-
-# create diagnostic plot
 # prepare data 
-df<- bind_rows(df_list, .id = "letter") %>% 
+df <- bind_rows(df_list, .id = "letter") %>% 
   pivot_longer(cols = c("temp", "rh"), names_to = "parameter")
  
 # create plot
-figure1 <- ggplot(data = df, aes(x = timePST, y = value, color= letter)) + 
+figs[["timeseries"]] <- ggplot(data = df, aes(x = time_PST, y = value, color= letter)) + 
   geom_line() + 
   facet_wrap(~ parameter, ncol = 1, scales = "free_y") +
   theme_bw()
 
-
-#Summary stats for temp + RH for entire experiment
-summary.stats <- df %>%
+# make summary stats table for temp + RH for entire experiment
+summary_stats <- df %>%
   group_by(parameter) %>%
   get_summary_stats()%>%
   select(parameter, n, mean, median, iqr)
 
-#making a visual table of summary stats
-sum_table <- ggtexttable(summary.stats, rows = NULL, theme = ttheme("blank")) %>%
+# make visual table of summary stats
+sum_table <- ggtexttable(summary_stats, rows = NULL, theme = ttheme("blank")) %>%
         tab_add_hline(at.row = 1:2, row.side = "top", linewidth = 2)
 
-#Creating combined plot using ggarrange #looks bad but room for improvement!
-figure2 <- ggarrange(fig, sum_table, heights = c(3, 3, 0.3), ncol = 1, nrow = 3)
+# create combined plot using ggarrange #looks bad but room for improvement!
+figs[["ts_table"]] <- ggarrange(figs[["timeseries"]], 
+                                sum_table, heights = c(3, 3, 0.3), ncol = 1, nrow = 3)
 
 
-
-# save 2 figures as ".png", ".jpg", or ".pdf" (and more) using lapply and ggsave
-# units can be specified as "in" or "cm"
-ggsave(filename = file.path(output_dir,"fig1.png"), plot=figure1, 
-       width = 8, height = 5, units = "in", dpi = "print")
-
-
-ggsave(filename = file.path(output_dir,"fig2.png"), plot=figure2, 
+# save figures as ".png", ".jpg", or ".pdf" (and more) using lapply and ggsave
+# units can be specified as "in", "cm"
+lapply(names(figs),function(x){
+  ggsave(filename = file.path(output_dir, paste0(x,".png")), plot = figs[[x]], 
          width = 8, height = 5, units = "in", dpi = "print")
-
+})
